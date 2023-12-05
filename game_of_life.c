9a15a4fc3058f4671d78b21408e1871306633c29
Artyom Rebrik <wildfrui@gmail.com>
2023-12-06 00:34:12 +0300

    fix

--- game_of_life.c
+++ game_of_life.c
@@ -1,62 +1,75 @@
 #include "game_of_life.h"
 
-int main() {
-    int matrix[ROWS][COLS];
-    int matrix_help[ROWS][COLS];
-    float speed = 1;
-    int delay = 50000, dead = 0;
-
-    input(matrix, ROWS, COLS);
-    FILE* file = freopen(FNAME, MODE, stdin);
-    initscr();
-    noecho();
-    wtimeout(stdscr, 2);
-    draw_generation(matrix_help, ROWS, COLS);
-    while (dead == 0) {
-        usleep(delay / speed);
-        check_input(&speed);
-        clear();
-        add_state(matrix, matrix_help, ROWS, COLS, &dead);
-        copy_matrix(matrix, matrix_help);
-        draw_generation(matrix_help, ROWS, COLS);
-        printw("\n");
-        printw("Speed %.1lf", speed);
-    }
-    fclose(file);
-    endwin();
-    return 0;
+int main()
+{
+    config cf = {.speed = 1.0, .delay = 50000, .game_finished = 0};
+    input(cf.matrix, ROWS, COLS);
+    start_game(cf);
+    return EXIT_SUCCESS;
 }
 
-void input(int matrix[ROWS][COLS], int rows, int cols) {
+void input(int matrix[ROWS][COLS], int rows, int cols)
+{
     int x;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if ((scanf("%d", &x))) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if ((scanf("%d", &x)))
+            {
                 matrix[i][j] = x;
             }
         }
     }
 }
 
-void draw_generation(int matrix[ROWS][COLS], int m, int n) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == 1) {
+void start_game(config cf) {
+    FILE * stream = safe_reopen();
+    init();
+    draw_generation(cf.matrix_help, ROWS, COLS);
+    while (cf.game_finished == 0)
+    {
+        usleep(cf.delay / cf.speed);
+        check_input(&cf.speed);
+        clear();
+        add_state(cf.matrix, cf.matrix_help, ROWS, COLS, &cf.game_finished);
+        copy_matrix(cf.matrix, cf.matrix_help);
+        draw_generation(cf.matrix_help, ROWS, COLS);
+        printw("\nSpeed %.1lf", cf.speed);
+    }
+    fclose(stream);
+    endwin();
+}
+
+void draw_generation(int matrix[ROWS][COLS], int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == 1)
+            {
                 printw("O");
-            } else {
+            }
+            else
+            {
                 printw(" ");
             }
         }
-        if (i != m - 1) {
+        if (i != m - 1)
+        {
             printw("\n");
         }
     }
 }
 
-int check_cell(int matrix[ROWS][COLS], int y, int x) {
+int check_cell(int matrix[ROWS][COLS], int y, int x)
+{
     int sum = 0, col, row;
-    for (int i = -1; i < 2; i++) {
-        for (int j = -1; j < 2; j++) {
+    for (int i = -1; i < 2; i++)
+    {
+        for (int j = -1; j < 2; j++)
+        {
             col = (x + i + COLS) % COLS;
             row = (y + j + ROWS) % ROWS;
             sum += matrix[row][col];
@@ -66,22 +79,31 @@ int check_cell(int matrix[ROWS][COLS], int y, int x) {
     return sum;
 }
 
-void add_state(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS], int rows, int cols, int* dead) {
+void add_state(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS], int rows, int cols, int *dead)
+{
     int sum_of_neighbours, cur_state;
     int flag = 1;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
             matrix_help[i][j] = 0;
             cur_state = matrix[i][j];
-            if (cur_state == 1) {
+            if (cur_state == 1)
+            {
                 flag = 0;
             }
             sum_of_neighbours = check_cell(matrix, i, j);
-            if (cur_state == 1 && (sum_of_neighbours == 2 || sum_of_neighbours == 3)) {
+            if (cur_state == 1 && (sum_of_neighbours == 2 || sum_of_neighbours == 3))
+            {
                 matrix_help[i][j] = 1;
-            } else if (cur_state == 0 && sum_of_neighbours == 3) {
+            }
+            else if (cur_state == 0 && sum_of_neighbours == 3)
+            {
                 matrix_help[i][j] = 1;
-            } else {
+            }
+            else
+            {
                 matrix_help[i][j] = 0;
             }
         }
@@ -89,21 +111,45 @@ void add_state(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS], int rows, in
     *dead = flag;
 }
 
-void copy_matrix(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS]) {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
+void copy_matrix(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
             matrix[i][j] = matrix_help[i][j];
         }
     }
 }
 
-void check_input(float* speed) {
+void check_input(float *speed)
+{
     char ch = getch();
-    if (ch == 'q' || ch == 'Q') {
+    if (ch == 'q' || ch == 'Q')
+    {
         exit(0);
-    } else if ((ch == 'm' || ch == 'M') && *speed < 2.9) {
+    }
+    else if ((ch == 'm' || ch == 'M') && *speed < 2.9)
+    {
         *speed += 0.1;
-    } else if ((ch == 'l' || ch == 'L') && *speed > 0.5) {
+    }
+    else if ((ch == 'l' || ch == 'L') && *speed > 0.5)
+    {
         *speed -= 0.1;
     }
+}
+
+void init() {
+    initscr();
+    noecho();
+    wtimeout(stdscr, 2);
+}
+
+void* safe_reopen() {
+    FILE* stream = freopen("/dev/tty", "r", stdin);
+    if (stream == NULL) {
+        printf("Cannot open /dev/tty\n");
+        exit(1);
+    }
+    return stream;
 }
--- game_of_life.h
+++ game_of_life.h
@@ -12,11 +12,23 @@
 #define FNAME "dev/tty"
 #define MODE "r"
 
+typedef struct config {
+    float speed;
+    int delay;
+    int game_finished;
+    int matrix[ROWS][COLS];
+    int matrix_help[ROWS][COLS];
+} config;
+
 void draw_generation(int matrix[ROWS][COLS], int m, int n);
 int check_cell(int matrix[ROWS][COLS], int y, int x);
 void add_state(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS], int rows, int cols, int* sum);
 void copy_matrix(int matrix[ROWS][COLS], int matrix_help[ROWS][COLS]);
 void input(int[ROWS][COLS], int rows, int cols);
 void check_input(float * speed);
+void init();
+void start_game(config cf);
+void* safe_reopen();
+void game_cycle(config * ncf);
 
 #endif
